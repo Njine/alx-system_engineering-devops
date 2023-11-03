1c6fa90f860024f7675db499342a676616a16bdc
njine <brikab@gmail.com>
2023-11-04 00:18:18 +0300

    smil

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,6 @@
 #!/usr/bin/env ruby
 
-input_string = ARGV[0]
-
-# Use the Oniguruma regular expression to match "School"
-regex = /School/
-
-# Check if the input string matches the regular expression
-if input_string.match(regex)
-  puts "School"
-else
-  puts ""
-end
-
+if ARGV.length == 1
+    puts ARGV[0][/School/]
+  end
+  
