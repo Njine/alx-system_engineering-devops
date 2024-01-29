9cc10cc649671660ee2e0532b0c5430d7b0f7641
njine <brikab@gmail.com>
2024-01-30 01:20:04 +0300

    ori

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,17 +10,43 @@ import sys
 
 def main():
     """Main function to retrieve and save employee information"""
-    url = 'https://jsonplaceholder.typicode.com/'
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <user_id>")
+        sys.exit(1)
 
     userid = sys.argv[1]
+
+    try:
+        int(userid)
+    except ValueError:
+        print("User ID must be an integer")
+        sys.exit(1)
+
+    url = 'https://jsonplaceholder.typicode.com/'
+
     user_endpoint = '{}users/{}'.format(url, userid)
     user_response = requests.get(user_endpoint)
+
+    if user_response.status_code != 200:
+        print("Error: Unable to retrieve user data")
+        sys.exit(1)
+
     user_data = user_response.json()
     username = user_data.get('username')
 
     todos_endpoint = '{}todos?userId={}'.format(url, userid)
     tasks_response = requests.get(todos_endpoint)
+
+    if tasks_response.status_code != 200:
+        print("Error: Unable to retrieve tasks data")
+        sys.exit(1)
+
     tasks_json = tasks_response.json()
+
+    if not isinstance(tasks_json, list) or not tasks_json:
+        print("No tasks found for the given user ID")
+        sys.exit(1)
+
     tasks_list = []
 
     for task in tasks_json:
