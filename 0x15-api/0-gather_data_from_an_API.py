f288db10084ed7992aa94609d767ff1249b50c07
njine <brikab@gmail.com>
2024-01-30 01:33:06 +0300

    oriu

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,57 @@
 #!/usr/bin/python3
-""" Retrieves employee information using JSONPlaceholder API """
-import json
+"""
+Retrieves employee TODO list progress using JSONPlaceholder API
+"""
+
 import requests
 import sys
 
 
-if __name__ == "__main__":
+def gather_todo_list_progress(employee_id):
+    """Retrieve and display employee TODO list progress"""
     url = 'https://jsonplaceholder.typicode.com/'
-    user_endpoint = '{}users'.format(url)
+
+    # Retrieve user data
+    user_endpoint = '{}users/{}'.format(url, employee_id)
     response = requests.get(user_endpoint)
-    users_json = response.json()
-    tasks_dict = {}
 
-    for user_data in users_json:
-        username = user_data.get('username')
-        userid = user_data.get('id')
-        todos_endpoint = '{}todos?userId={}'.format(url, userid)
-        tasks_response = requests.get(todos_endpoint)
-        tasks_json = tasks_response.json()
-        user_tasks = []
+    if response.status_code != 200:
+        print("Error: Unable to retrieve user data")
+        sys.exit(1)
 
-        for task in tasks_json:
-            task_dict = {"username": username,
-                         "task": task.get('title'),
-                         "completed": task.get('completed')}
-            user_tasks.append(task_dict)
+    user_data = response.json()
+    username = user_data.get('username')
 
-        tasks_dict[str(userid)] = user_tasks
+    # Retrieve tasks data
+    todos_endpoint = '{}todos?userId={}'.format(url, employee_id)
+    tasks_response = requests.get(todos_endpoint)
 
-    filename = 'todo_all_employees.json'
-    with open(filename, mode='w') as file:
-        json.dump(tasks_dict, file)
+    if tasks_response.status_code != 200:
+        print("Error: Unable to retrieve tasks data")
+        sys.exit(1)
+
+    tasks_json = tasks_response.json()
+    total_tasks = len(tasks_json)
+    completed_tasks = sum(task.get('completed') for task in tasks_json)
+
+    # Display TODO list progress
+    print("Employee {} is done with tasks({}/{}):".format(username, completed_tasks, total_tasks))
+    for task in tasks_json:
+        if task.get('completed'):
+            print("\t{}".format(task.get('title')))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+
+    try:
+        employee_id = int(employee_id)
+    except ValueError:
+        print("Error: Employee ID must be an integer")
+        sys.exit(1)
+
+    gather_todo_list_progress(employee_id)
