03f8cb3ca3e3ff6e74792af27bd443aff95b0958
njine <brikab@gmail.com>
2024-01-31 02:24:39 +0300

    revo

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,52 +1,37 @@
 #!/usr/bin/python3
-"""
-Retrieves employee information using JSONPlaceholder API and writes to CSV
-"""
 
-import csv
+'''Retrieves employee data from API using employee ID.'''
+
 import requests
 import sys
 
+def fetch_data(employee_id):
+    '''Retrieves employee data using ID: [name, ID, [completed_tasks], [all_tasks]].'''
+    data = ['', 0, [], []]
+    user = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}').json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    data[0] = user.get('name')
+    data[1] = user.get('id')
+    for todo in todos:
+        if todo.get('userId') == user.get('id'):
+            data[3].append(todo)
+            if todo.get('completed'):
+                data[2].append(todo)
+    return data
 
 def main():
-    """Main function to retrieve and save employee information to CSV"""
+    '''Main function.'''
     if len(sys.argv) != 2:
-        print("Usage: python script.py <user_id>")
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    url = 'https://jsonplaceholder.typicode.com/'
-
-    userid = sys.argv[1]
-    user_endpoint = '{}users/{}'.format(url, userid)
-    response = requests.get(user_endpoint)
-
-    if response.status_code != 200:
-        print("Error: Unable to retrieve user data")
-        sys.exit(1)
-
-    user_data = response.json()
-    username = user_data.get('username')
-
-    todos_endpoint = '{}todos?userId={}'.format(url, userid)
-    tasks_response = requests.get(todos_endpoint)
-
-    if tasks_response.status_code != 200:
-        print("Error: Unable to retrieve tasks data")
-        sys.exit(1)
-
-    tasks_json = tasks_response.json()
-
-    if not isinstance(tasks_json, list) or not tasks_json:
-        print("No tasks found for the given user ID")
-        sys.exit(1)
-
-    filename = '{}.csv'.format(userid)
-    with open(filename, mode='w', newline='') as employee_file:
-        employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        employee_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in tasks_json:
-            employee_writer.writerow([userid, username, task.get('completed'), task.get('title')])
+    employee_id = int(sys.argv[1])
+    data = fetch_data(employee_id)
+    print(f'Employee {data[0]} is done with tasks({len(data[2])}/{len(data[3])}):')
+    for todo in data[2]:
+        print(f'\t{todo.get("title")}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
