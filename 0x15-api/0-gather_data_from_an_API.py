fb3a983f7124f383dd613aeb1fb9ced9fdcc502a
njine <brikab@gmail.com>
2024-01-31 02:27:08 +0300

    revo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-
-'''Retrieves employee data from API using employee ID.'''
-
+'''Gathers employee data from API using ID.'''
 import requests
-import sys
+from sys import argv
 
 
-def fetch_data(employee_id):
-    '''Gets employee data: [name, ID, [completed_tasks], [all_tasks]].'''
+def fetch_data(id):
+    '''Fetches employee data using ID. Format: [name, ID, [completed_tasks], [all_tasks]].'''
     data = ['', 0, [], []]
-    try:
-        user = requests.get(
-            f'https://jsonplaceholder.typicode.com/users/{employee_id}').json()
-        todos = requests.get(
-            'https://jsonplaceholder.typicode.com/todos').json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(
+        id)).json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    data[0] = user.get('name')
+    data[1] = user.get('id')
+    for todo in todos:
+        if todo.get('userId') == user.get('id'):
+            data[3].append(todo)
+            if todo.get('completed'):
+                data[2].append(todo)
+    return data
+
+
+if __name__ == '__main__':
+    employee_id = argv[1]
+    data = fetch_data(employee_id)
+    print('Employee {} is done with tasks({}/{}):'.format(
+        data[0], len(data[2]), len(data[3])))
+    for todo in data[2]:
+        print('\t{}'.format(todo.get('title')))
