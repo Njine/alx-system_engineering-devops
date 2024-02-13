155c80c4caefbf8771d1242932c25c0b19b0850e
njine <brikab@gmail.com>
2024-02-13 23:26:28 +0300

    spam

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,7 +8,7 @@ import sys
 
 
 def add_title(hot_list, hot_posts):
-    """ Adds item into a list """
+    """Adds item into a list"""
     if len(hot_posts) == 0:
         return
     hot_list.append(hot_posts[0]['data']['title'])
@@ -17,20 +17,19 @@ def add_title(hot_list, hot_posts):
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """ Queries the Reddit API recursively """
+    """Queries the Reddit API recursively"""
     user_agent = 'Mozilla/5.0'
     headers = {'User-Agent': user_agent}
     params = {'after': after}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
+    # Send a GET request to the Reddit API
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
-# Send a GET request to the Reddit API
-response = requests.get(url, headers=headers, params=params,
-                        allow_redirects=False)
-
-# If the request fails, return None
-if response.status_code != 200:
-    return None
+    # If the request fails, return None
+    if response.status_code != 200:
+        return None
 
     # Extract data from the response
     data = response.json()['data']
@@ -44,3 +43,12 @@ if response.status_code != 200:
 
     # Recursively call the function to fetch more posts
     return recurse(subreddit, hot_list=hot_list, after=after)
+
+
+# Check if the code meets the requirements
+if __name__ == '__main__':
+    result = recurse(sys.argv[1])
+    if result is not None:
+        print(len(result))
+    else:
+        print("None")
